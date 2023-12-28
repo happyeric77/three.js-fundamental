0d3c438d1c7fe5df1876bbe473d60c816d62139d
Eric Lee <macbookpro4eric@Mac-mini.local>
2023-12-28 10:48:27 +0900

    Add Open3D concept

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,10 +6,42 @@ import { GUI } from "dat.gui";
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+const debug = document.getElementById("debug1") as HTMLDivElement;
+
 const animate = (render: THREE.WebGLRenderer) => {
   requestAnimationFrame(() => animate(render));
   stats.update();
   render.render(scene, camera);
+
+  // Update debug console
+  const object1WorldPosition = new THREE.Vector3();
+  object1.getWorldPosition(object1WorldPosition);
+  const object2WorldPosition = new THREE.Vector3();
+  object2.getWorldPosition(object2WorldPosition);
+  const object3WorldPosition = new THREE.Vector3();
+  object3.getWorldPosition(object3WorldPosition);
+  debug.innerText =
+    "Red\n" +
+    "Local Pos X : " +
+    object1.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object1WorldPosition.x.toFixed(2) +
+    "\n" +
+    "\nGreen\n" +
+    "Local Pos X : " +
+    object2.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object2WorldPosition.x.toFixed(2) +
+    "\n" +
+    "\nBlue\n" +
+    "Local Pos X : " +
+    object3.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object3WorldPosition.x.toFixed(2) +
+    "\n";
 };
 
 const onWindowResize = () => {
@@ -38,32 +70,68 @@ const camera = new THREE.PerspectiveCamera(
 
 const control = new OrbitControls(camera, renderer.domElement);
 control.addEventListener("change", () => renderer.render(scene, camera));
+// control.target.set(5, 5, 5);
 
-camera.position.set(1, 2, 5);
+camera.position.set(8, 4, 4);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-  color: "pink",
-  wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
+// Objects and Lights
 
-scene.add(cube);
+const light1 = new THREE.PointLight(0xffffff, 400);
+light1.position.set(10, 10, 10);
+scene.add(light1);
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+const light2 = new THREE.PointLight(0xffffff, 400);
+light2.position.set(-10, 10, 10);
+scene.add(light2);
+
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshPhongMaterial({ color: "pink" })
+);
+scene.add(object1);
+scene.add(new THREE.AxesHelper(5));
+
+object1.position.set(4, 0, 0);
+object1.add(new THREE.AxesHelper(5));
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshPhongMaterial({ color: "white" })
+);
+
+object1.add(object2);
+object2.position.set(4, 0, 0);
+object2.add(new THREE.AxesHelper(5));
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshPhongMaterial({ color: "green" })
+);
+object3.position.set(4, 0, 0);
+
+object2.add(object3);
+object3.add(new THREE.AxesHelper(5));
 
 animate(renderer);
 renderer.render(scene, camera);
 
+// dat.gui
 const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubeFolder.open();
-const cameraFolder = gui.addFolder("Camera");
-cameraFolder.add(camera.position, "x", -10, 10);
-cameraFolder.add(camera.position, "y", -10, 10);
-cameraFolder.add(camera.position, "z", -10, 10);
-cameraFolder.open();
+
+const object1Folder = gui.addFolder("Object1");
+object1Folder.add(object1.position, "x", -10, 10).name("x position");
+object1Folder.add(object1.rotation, "x", 0, Math.PI * 2).name("x rotation");
+object1Folder.add(object1.scale, "x", 0, 10).name("x scale");
+object1Folder.open();
+
+const object2Folder = gui.addFolder("Object2");
+object2Folder.add(object2.position, "x", -10, 10).name("x position");
+object2Folder.add(object2.rotation, "x", 0, Math.PI * 2).name("x rotation");
+object2Folder.add(object2.scale, "x", 0, 10).name("x scale");
+object2Folder.open();
+
+const object3Folder = gui.addFolder("Object3");
+object3Folder.add(object3.position, "x", -10, 10).name("x position");
+object3Folder.add(object3.rotation, "x", 0, Math.PI * 2).name("x rotation");
+object3Folder.add(object3.scale, "x", 0, 10).name("x scale");
+object3Folder.open();
